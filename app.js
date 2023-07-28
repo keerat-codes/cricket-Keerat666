27aa7aef88c5fc1e46a3cb5c09bc8867aa7b19b9
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-07-29 02:19:40 +0530

    Updated App.js for cricketers route using AutoPilot

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = 8009;
 
 require('dotenv').config();
 
+const cricketersRoute = require('./routes/cricketers');
+app.use('/cricketers', cricketersRoute);
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
